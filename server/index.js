d5563e88908674c6856dc110cef0c62e014dadac
sourav225 <68838824+sourav225@users.noreply.github.com>
2024-03-18 14:30:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:["https://vercel-deployment1-client.vercel.app","https://vercel-deployment1-client.vercel.app/display"],
+  origin:["https://vercel-deployment1-client.vercel.app"],
   methods:["GET","PUT","POST","DELETE"],
   credentials:true
 }));
