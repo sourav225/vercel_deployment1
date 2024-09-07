dd3d396b131ac8b935ed8682c4f67758848cf7d8
sourav225 <68838824+sourav225@users.noreply.github.com>
2024-09-07 16:20:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,10 +16,7 @@ app.options('*', cors());
 
 const PORT = 3550;
 
-mongoose.connect("mongodb+srv://souravlayekjsr100:Sourav%40123@cluster0.yhf674r.mongodb.net/Project?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb+srv://souravlayekjsr100:Sourav%40123@cluster0.yhf674r.mongodb.net/Project?retryWrites=true&w=majority");
 
 
 app.get("/display", async (req, res) => {
